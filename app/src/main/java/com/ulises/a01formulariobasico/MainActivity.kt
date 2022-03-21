0bf461baf9b35e69e6e4cf274f72e4217c3c2c35
JoseUlisesCeron <00164319@uca.edu.sv>
2022-03-20 22:15:38 -0600

    fui fui

---
+++ app/src/main/java/com/ulises/a01formulariobasico/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ulises.a01formulariobasico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
